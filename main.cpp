0b4fd8e5d527b8cc6293d2d595df68b0415e754c
atg99 <atg9910@naver.com>
2025-09-16 11:01:39 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,21 @@ int main()
 	int EXP = 0;
 	int GetEXP = 4;
 	int NeedEXP = 10;
-	while (EXP <= NeedEXP)
+	int Level = 1;
+	while (Level < 10)
 	{
-		EXP += GetEXP;
-		cout << "GetEXP + ";
-		cout << GetEXP << endl;
+		while (EXP <= NeedEXP)
+		{
+			EXP += GetEXP;
+			cout << "GetEXP + ";
+			cout << GetEXP << endl;
+		}
+		EXP = 0;
+		NeedEXP *= 2;
+		Level += 1;
+		cout << "level up" << endl;
+		cout << "level : "; cout << Level << endl;
 	}
-	EXP = 0;
-	NeedEXP *= 2;
-	cout << "level up" << endl;
+
 	return 0;
 }
