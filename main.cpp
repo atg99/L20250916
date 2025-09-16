c2a9e7637254a4a1e870418293abbaa6b6985067
atg99 <atg9910@naver.com>
2025-09-16 10:47:27 +0900

    commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main() 
 {
-
-
+	int EXP = 0;
+	int GetEXP = 4;
+	int NeedEXP = 10;
+	while (EXP >= NeedEXP)
+	{
+		EXP += GetEXP;
+	}
+	NeedEXP *= 2;
+	cout << "level up" << endl;
 	return 0;
 }
